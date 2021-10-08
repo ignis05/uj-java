5ad43c1f369f7c4966e68e0c33707f9894f89768
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-10-08 10:17:42 +0200

    feat: wyk1 gcd

---
+++ wyk1/GCD.java
@@ -0,0 +1,18 @@
+class GCD {
+  public static int gcd(int a, int b) {
+    if (a == 0)
+      return b;
+    return gcd(b % a, a);
+  }
+
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.out.println("Not enough arguments");
+      return;
+    }
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    int result = gcd(a, b);
+    System.out.println("Result: " + result);
+  }
+}
