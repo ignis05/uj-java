3ad223a4eadf47ef2874c070af171d580ea70b26
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-10-14 16:08:57 +0200

    feat: better test

--- zad01/Test.java
+++ zad01/Test.java
@@ -1,4 +1,6 @@
 public class Test {
+  public static int errorCount;
+
   public static String testString(Decode decoder, String test) {
     for (char ch : test.toCharArray()) {
       int bit = Character.getNumericValue(ch);
@@ -12,16 +14,30 @@ public class Test {
 
   public static String testString(Decode decoder, String test, String expected) {
     String result = testString(decoder, test);
-    if (!result.equals(expected))
+    if (!result.equals(expected)) {
+      errorCount++;
       System.err.println("Error: " + test + " => " + result + " || expected: " + expected);
+    }
     return result;
   }
 
   public static void main(String[] args) {
     Decode decoder = new Decode();
 
+    errorCount = 0;
     testString(decoder, "000010000110010111010", "01020");
     testString(decoder, "11011110000000011011111100110", "01020");
+    testString(decoder, "", "");
+    testString(decoder, "00000111111111", "");
+    testString(decoder, "10", "0");
+    testString(decoder, "10110111011110", "0123");
+    testString(decoder, "110111101111110111111110", "0123");
+    testString(decoder, "00000000000000011000000000000011110000000000000011111100000000001111111100000000000", "0123");
+
+    if (errorCount == 0)
+      System.out.println("All tests passed");
+    else
+      System.out.println(errorCount + " tests failed");
   }
 
 }
