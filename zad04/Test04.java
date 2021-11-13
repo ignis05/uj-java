9ffc35b2399ac957fa0309674672af0bb63fac99
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-11-13 02:00:57 +0100

    feat(04): tester

--- zad04/Test04.java
+++ zad04/Test04.java
@@ -1,4 +1,12 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Test04 {
+  static int errorCount = 0;
+
   public static void main(String[] args) {
     var b = new BusLine();
     b.addBusLine("a", new Position2D(1, 1), new Position2D(2, 6));
@@ -15,12 +23,58 @@ public class Test04 {
     b.addLineSegment("c", new LineSegment(new Position2D(8, 2), new Position2D(4, 2)));
 
     b.findIntersections();
-    System.out.println(b.getLines());
-    System.out.println("\n");
-    System.out.println(b.getIntersectionPositions());
-    System.out.println("\n");
-    System.out.println(b.getIntersectionsWithLines());
-    System.out.println("\n");
-    System.out.println(b.getIntersectionOfLinesPair());
+    
+    // getLines
+    Map<String, List<Position>> expLines = new HashMap<String, List<Position>>();
+    expLines.put("a", List.of(new Position2D(1, 1), new Position2D(2, 2), new Position2D(3, 3), new Position2D(4, 4),
+        new Position2D(5, 5), new Position2D(6, 6), new Position2D(7, 7), new Position2D(7, 6), new Position2D(7, 5),
+        new Position2D(7, 4), new Position2D(7, 3), new Position2D(7, 2), new Position2D(7, 1), new Position2D(6, 2),
+        new Position2D(5, 3), new Position2D(4, 4), new Position2D(3, 5), new Position2D(2, 6)));
+    expLines.put("c", List.of(new Position2D(1, 1), new Position2D(1, 2), new Position2D(1, 3), new Position2D(1, 4),
+        new Position2D(2, 4), new Position2D(3, 4), new Position2D(4, 4), new Position2D(5, 4), new Position2D(6, 4),
+        new Position2D(7, 4), new Position2D(8, 4), new Position2D(8, 3), new Position2D(8, 2), new Position2D(7, 2),
+        new Position2D(6, 2), new Position2D(5, 2), new Position2D(4, 2)));
+    if (!expLines.equals(b.getLines())) {
+      System.err.println("error: getLines invalid");
+      errorCount++;
+    }
+
+    // getIntersectionPositions
+    Map<String, List<Position>> expPositions = new HashMap<String, List<Position>>();
+    expPositions.put("a",
+        List.of(new Position2D(4, 4), new Position2D(7, 4), new Position2D(7, 2), new Position2D(4, 4)));
+    expPositions.put("c", List.of(new Position2D(7, 4), new Position2D(7, 2)));
+    if (!expPositions.equals(b.getIntersectionPositions())) {
+      System.err.println("error: getIntersectionPositions invalid");
+      errorCount++;
+    }
+
+    // getIntersectionsWithLines
+    Map<String, List<String>> expInter = new HashMap<String, List<String>>();
+    expInter.put("a", List.of("a", "c", "c", "a"));
+    expInter.put("c", List.of("a", "a"));
+    if (!expInter.equals(b.getIntersectionsWithLines())) {
+      System.err.println("error: getIntersectionsWithLines invalid");
+      errorCount++;
+    }
+
+    // getIntersectionsWithLines
+    Map<BusLine.LinesPair, Set<Position>> expPair = new HashMap<BusLine.LinesPair, Set<Position>>();
+    expPair.put(b.new LinesPair("b", "a"), new HashSet<Position>(List.of()));
+    expPair.put(b.new LinesPair("c", "b"), new HashSet<Position>(List.of()));
+    expPair.put(b.new LinesPair("a", "a"), new HashSet<Position>(List.of(new Position2D(4, 4))));
+    expPair.put(b.new LinesPair("b", "b"), new HashSet<Position>(List.of()));
+    expPair.put(b.new LinesPair("c", "c"), new HashSet<Position>(List.of()));
+    expPair.put(b.new LinesPair("a", "b"), new HashSet<Position>(List.of()));
+    expPair.put(b.new LinesPair("b", "c"), new HashSet<Position>(List.of()));
+    expPair.put(b.new LinesPair("a", "c"), new HashSet<Position>(List.of(new Position2D(7, 2), new Position2D(7, 4))));
+    expPair.put(b.new LinesPair("c", "a"), new HashSet<Position>(List.of(new Position2D(7, 2), new Position2D(7, 4))));
+    if (!expPair.equals(b.getIntersectionOfLinesPair())) {
+      System.err.println("error: getIntersectionsWithLines invalid");
+      errorCount++;
+    }
+
+    if (errorCount == 0)
+      System.out.println("all tests passed");
   }
 }
