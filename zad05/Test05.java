2ca1912b5422915ff66a2440d308da67a082e49f
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-11-21 00:12:23 +0100

    05: more tests

--- zad05/Test05.java
+++ zad05/Test05.java
@@ -52,8 +52,7 @@ public class Test05 {
 
   }
 
-  public static void main(String[] args)
-      throws GraphicsInterface.WrongStartingPosition, GraphicsInterface.NoCanvasException {
+  public static void main(String[] args) throws GraphicsInterface.WrongStartingPosition, GraphicsInterface.NoCanvasException {
     var canvas = new Canvas(new Color[][] {
         { Color.WHITE, Color.RED, Color.RED, Color.WHITE, Color.RED, Color.WHITE, Color.RED, Color.WHITE, Color.WHITE,
             Color.WHITE, },
@@ -97,7 +96,7 @@ public class Test05 {
     }
     System.out.println("test 1 " + (threwEx ? "passed" : "failed"));
 
-    // test 2 - invalid pos (out of canvas)
+    // test 2 - invalid pos (out od canvas)
     threwEx = false;
     gp.setCanvas(canvas);
     try {
@@ -107,7 +106,7 @@ public class Test05 {
     }
     System.out.println("test 2 " + (threwEx ? "passed" : "failed"));
 
-    // test 3 - invalid pos (in border)
+    // test 3 - invalid pos (pos in border)
     threwEx = false;
     gp.setCanvas(canvas);
     try {
@@ -121,7 +120,7 @@ public class Test05 {
         sb.append("\n");
       }
       String expectedStr = "WRRWRWRWWW\nWRRRRWRWWW\nWRWWWWWRWW\nWRWWWWWWRW\nRWWWWWWWRW\nWRWWWWWWRW\nRWWWWWWWRW\nWRRRWWRRWW\nWWWWRRWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\n";
-      threwEx = expectedStr.equals(sb.toString());
+      threwEx = expectedStr.equals(sb.toString()) ? true: false;
     }
     System.out.println("test 3 " + (threwEx ? "passed" : "failed"));
 
@@ -137,5 +136,45 @@ public class Test05 {
     // System.out.println(sb.toString());
     String expectedStr = "WRRWRGRWWW\nWRRRRGRWWW\nWRGGGGGRWW\nWRGGGGGGRW\nRGGGGGGGRW\nWRGGGGGGRW\nRGGGGGGGRW\nWRRRGGRRWW\nWWWWRRWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\n";
     System.out.println("test 4 " + (expectedStr.equals(sb.toString()) ? "passed" : "failed"));
+
+    // test 5
+    gp.fillWithColor(new Position2(5, 5), Color.ORANGE);
+    sb = new StringBuilder();
+    for (int i = 0; i < canvas.array.length; i++) {
+      for (int j = 0; j < canvas.array[0].length; j++) {
+        sb.append(canvas.array[i][j]);
+      }
+      sb.append("\n");
+    }
+    // System.out.println(sb.toString());
+    expectedStr = "WRRWRORWWW\nWRRRRORWWW\nWROOOOORWW\nWROOOOOORW\nROOOOOOORW\nWROOOOOORW\nROOOOOOORW\nWRRROORRWW\nWWWWRRWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\nWWWWWWWWWW\n";
+    System.out.println("test 5 " + (expectedStr.equals(sb.toString()) ? "passed" : "failed"));
+
+    // test 6
+    canvas = new Canvas(new Color[][] {
+    { Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, },
+    { Color.RED, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.RED, },
+    { Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.WHITE, Color.RED, },
+    { Color.RED, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.RED, },
+    { Color.RED, Color.WHITE, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, },
+    { Color.RED, Color.WHITE, Color.RED, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.RED, },
+    { Color.RED, Color.WHITE, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.WHITE, Color.RED, },
+    { Color.RED, Color.WHITE, Color.RED, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.RED, Color.WHITE, Color.RED, },
+    { Color.RED, Color.WHITE, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.WHITE, Color.RED, },
+    { Color.RED, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.RED, },
+    { Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, },});
+
+    gp.setCanvas(canvas);
+    gp.fillWithColor(new Position2(1, 1), Color.GREEN);
+    sb = new StringBuilder();
+    for (int i = 0; i < canvas.array.length; i++) {
+      for (int j = 0; j < canvas.array[0].length; j++) {
+        sb.append(canvas.array[i][j]);
+      }
+      sb.append("\n");
+    }
+    // System.out.println(sb.toString());
+    expectedStr = "RRRRRRRRRRR\nRGGGGGGGGGR\nRRRRRRRRRGR\nRGGGGGGGGGR\nRGRRRRRRRRR\nRGRGGGGGGGR\nRGRRRRRRRGR\nRGRWWWWWRGR\nRGRRRRRRRGR\nRGGGGGGGGGR\nRRRRRRRRRRR\n";
+    System.out.println("test 6 " + (expectedStr.equals(sb.toString()) ? "passed" : "failed"));
   }
 }
