c06a1a1d3f0d58e596a1085e2bd0c790f6ff7509
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-11-21 21:44:09 +0100

    04: remove obsolete imports

--- zad04/TesterOramusa.java
+++ zad04/TesterOramusa.java
@@ -1,8 +1,6 @@
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class TesterOramusa {
   static int errorCount = 0;
