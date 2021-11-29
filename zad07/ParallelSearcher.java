e18ffd9daa6a2ad59d16cba05ba65bc8f6e3b759
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-11-30 00:00:51 +0100

    07: starting files

---
+++ zad07/ParallelSearcher.java
@@ -0,0 +1,9 @@
+class ParallelSearcher implements ParallelSearcherInterface{
+
+  @Override
+  public void set(HidingPlaceSupplierSupplier supplier) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
