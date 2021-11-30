97c3e968bd2c5c122c4a3cc051d201dfea6fc6e3
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-11-30 11:31:27 +0100

    07: improved tester

--- zad07/ParallelSearcher.java
+++ zad07/ParallelSearcher.java
@@ -2,23 +2,33 @@
 class ParallelSearcher implements ParallelSearcherInterface {
 
   class BoxOpener extends Thread {
+    private boolean printLogs = false;
+
+    public BoxOpener(boolean printLogs) {
+      this.printLogs = printLogs;
+    }
+
     @Override
     public void run() {
       HidingPlaceSupplier.HidingPlace box;
       while ((box = supplier.get()) != null) {
-        System.out.println(Thread.currentThread().getName() + " retrieved box");
+        if (printLogs)
+          System.out.println(Thread.currentThread().getName() + " retrieved box");
         if (box.isPresent()) {
           double value;
           synchronized (supplier) {
-            System.out.println(Thread.currentThread().getName() + "'s box is full, opening (total=" + totalValue + ")");
+            if (printLogs)
+              System.out.println(Thread.currentThread().getName() + "'s box is full, opening (total=" + totalValue + ")");
             value = box.openAndGetValue();
-            System.out.println(Thread.currentThread().getName() + "'s box's value is: " + value);
+            if (printLogs)
+              System.out.println(Thread.currentThread().getName() + "'s box's value is: " + value);
           }
           synchronized (totalValue) {
             totalValue += value;
-            System.out.println(Thread.currentThread().getName() + "'s box's value (" + value + ") added to total: " + totalValue);
+            if (printLogs)
+              System.out.println(Thread.currentThread().getName() + "'s box's value (" + value + ") added to total: " + totalValue);
           }
-        } else
+        } else if (printLogs)
           System.out.println(Thread.currentThread().getName() + "'s box is empty");
       }
     }
@@ -26,17 +36,19 @@ class ParallelSearcher implements ParallelSearcherInterface {
 
   HidingPlaceSupplier supplier;
   Double totalValue = 0.;
+  boolean printLogs = false;
 
   @Override
   public void set(HidingPlaceSupplierSupplier HPSSupplier) {
+    int counter = 0;
     while ((supplier = HPSSupplier.get(totalValue)) != null) {
-      System.out.println("Getting new HPS");
+      System.out.println("=== Retrieved new HPS (" + counter++ + ") ===");
       // reset value
       totalValue = 0.;
       // launch searching threads
       BoxOpener[] openers = new BoxOpener[supplier.threads()];
       for (int i = 0; i < supplier.threads(); i++) {
-        var opener = new BoxOpener();
+        var opener = new BoxOpener(printLogs);
         opener.setName("Opener " + i);
         opener.start();
         openers[i] = opener;
--- zad07/Test07.java
+++ zad07/Test07.java
@@ -1,16 +1,28 @@
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+
 public class Test07 {
+  static Random random = new Random();
+
   public static class HPS implements HidingPlaceSupplier {
+    private AtomicBoolean isOpening = new AtomicBoolean(false); // if a place is being opened atm
+    private AtomicInteger detectedThreads = new AtomicInteger(0);
+    private AtomicInteger maxDetThreads = new AtomicInteger(0);
     private int threads;
-    private int[] values;
+    private List<Integer> values;
     private Integer i = 0;
 
-    public HPS(int threads, int... values) {
-      this.threads = threads;
+    public HPS(List<Integer> values) {
+      this.threads = values.remove(0);
       this.values = values;
     }
 
     class HP implements HidingPlace {
       private int value;
+      private boolean isOpened = false;
 
       public HP(int value) {
         this.value = value;
@@ -18,21 +30,34 @@ public class Test07 {
 
       @Override
       public boolean isPresent() {
+        // check thread limit
+        if (detectedThreads.incrementAndGet() > threads)
+          throw new RuntimeException("Detected more threads than maximum allowed");
+        // save max thread count
+        if (detectedThreads.get() > maxDetThreads.get())
+          maxDetThreads.set(detectedThreads.get());
         try {
-          Thread.sleep(1000);
+          Thread.sleep(100 + random.nextInt(1000));
         } catch (Exception e) {
           System.err.println(e);
         }
+        detectedThreads.decrementAndGet();
         return this.value != 0;
       }
 
       @Override
       public double openAndGetValue() {
+        if (!isOpening.compareAndSet(false, true))
+          throw new RuntimeException("Attempted to open multiple boxes at the same time");
+        if (this.isOpened)
+          throw new RuntimeException("Attempted to open the same box more than once");
         try {
-          Thread.sleep(200);
+          Thread.sleep(100 + random.nextInt(200));
         } catch (Exception e) {
           System.err.println(e);
         }
+        isOpening.set(false);
+        this.isOpened = true;
         return this.value;
       }
 
@@ -41,10 +66,13 @@ public class Test07 {
     @Override
     public HidingPlaceSupplier.HidingPlace get() {
       synchronized (i) {
-        if (i < this.values.length)
-          return new HP(this.values[i++]);
-        else
+        if (i < this.values.size())
+          return new HP(this.values.get(i++));
+        else {
+          if (maxDetThreads.get() < this.threads)
+            throw new RuntimeException("Less concurent threads detected than the expected thread count");
           return null;
+        }
       }
     }
 
@@ -55,27 +83,39 @@ public class Test07 {
 
   }
 
-  // todo: constuctor that takes all HPS values
   public static class HPSS implements HidingPlaceSupplierSupplier {
+    List<List<Integer>> hpsValues;
+    double expValue = 0;
+
+    HPSS(List<List<Integer>> hpsValues) {
+      this.hpsValues = hpsValues;
+    }
+
+    public boolean isEmpty() {
+      return this.hpsValues.size() == 0;
+    }
 
     @Override
     public HidingPlaceSupplier get(double totalValueOfPreviousObject) {
-      if (totalValueOfPreviousObject == 0) {
-        return new HPS(3, 5, 0, 4, 0, 1, 0, 0);
-      } else if (totalValueOfPreviousObject == 10) {
-        return new HPS(4, 0, 2, 0, 0, 0, 0, 0, 1, 3, 7);
-      } else if (totalValueOfPreviousObject == 13) {
-        System.out.println("Succesfully reached HPSS end");
-        return null;
+      if (totalValueOfPreviousObject == expValue) {
+        if (hpsValues.size() == 0)
+          return null;
+        var hpValues = hpsValues.remove(0);
+        expValue = hpValues.stream().mapToInt(Integer::intValue).sum() - hpValues.get(0); // subtract thread count
+        return new HPS(hpValues);
       } else
-        throw new RuntimeException("invalid totalValueOfPreviousObject requested");
+        throw new RuntimeException("Invalid totalValueOfPreviousObject:\nExpected: " + expValue + ", Received: " + totalValueOfPreviousObject);
     }
 
   }
 
   public static void main(String[] args) {
-    var hpss = new HPSS();
+    List<List<Integer>> hpsValues = new ArrayList<List<Integer>>(2);
+    hpsValues.add(new ArrayList<Integer>(List.of(3, 5, 0, 4, 0, 1, 0, 0)));
+    hpsValues.add(new ArrayList<Integer>(List.of(4, 0, 2, 0, 0, 0, 0, 0, 1, 3, 7)));
+    var hpss = new HPSS(hpsValues);
     var searcher = new ParallelSearcher();
     searcher.set(hpss);
+    System.out.println(hpss.isEmpty());
   }
 }
