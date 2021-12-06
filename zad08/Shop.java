c7b2ca2193660b970819f6e85ca63bb9b0c22c17
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-12-06 15:37:27 +0100

    08: source files

---
+++ zad08/Shop.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+
+class Shop implements ShopInterface {
+
+  @Override
+  public void delivery(Map<String, Integer> goods) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean purchase(String productName, int quantity) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Map<String, Integer> stock() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
