f30bee8dcac65a07249e58410d316e0de09af4e3
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-12-06 16:16:30 +0100

    08: first draft of shop

--- zad08/Shop.java
+++ zad08/Shop.java
@@ -1,23 +1,57 @@
+import java.util.HashMap;
 import java.util.Map;
 
 class Shop implements ShopInterface {
+  private Map<String, Integer> stock = new HashMap<String, Integer>();
 
   @Override
   public void delivery(Map<String, Integer> goods) {
-    // TODO Auto-generated method stub
+    synchronized (this.stock) {
+      for (var item : goods.entrySet()) {
+        String key = item.getKey();
+        Integer value = item.getValue();
+        if (this.stock.containsKey(item.getKey()))
+          this.stock.put(key, this.stock.get(key) + value);
+        else
+          this.stock.put(key, value);
 
+        stock.get(key).notifyAll();
+      }
+    }
   }
 
   @Override
   public boolean purchase(String productName, int quantity) {
-    // TODO Auto-generated method stub
-    return false;
+    boolean firstTry = true;
+    synchronized (this.stock) {
+      while (true) {
+        // if stock contains key
+        if (this.stock.containsKey(productName)) {
+          var value = this.stock.get(productName);
+          // if there is enough stock
+          if (value <= quantity) {
+            value -= quantity;
+            return true;
+          }
+          // if failed to buy in the first try, wait and try agaim
+          if (firstTry) {
+            firstTry = false;
+            try {
+              value.wait();
+            } catch (InterruptedException e) {
+            }
+            continue;
+          }
+          return false;
+        } else // if stock doesnt contain key - ???
+          return false;
+      }
+    }
   }
 
   @Override
   public Map<String, Integer> stock() {
-    // TODO Auto-generated method stub
-    return null;
+    return this.stock;
   }
 
 }
