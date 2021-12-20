c81362fe171661f04677001f92fa41da9e3c34d2
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-12-20 22:24:57 +0100

    zad10: init

---
+++ zad10/Kasjer.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.function.Supplier;
+
+class Kasjer implements KasjerInterface{
+
+  @Override
+  public List<Pieniadz> rozlicz(int cena, List<Pieniadz> pieniadze) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<Pieniadz> stanKasy() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void dostępDoRozmieniacza(RozmieniaczInterface rozmieniacz) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void dostępDoPoczątkowegoStanuKasy(Supplier<Pieniadz> dostawca) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
